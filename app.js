7cc2e3324211f8bf5c0f164bd2a9b81970e2d4f3
yuval hadar <yuvalhad12@gmail.com>
2023-07-14 18:53:21 +0300

    last changes. project sealed and working according to guide

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 
-const todos = ["Add your wanted item by", "Clicking on the +, and remove", "By checking the box"];
-
+const todos_main = ["Add your wanted item by", "Clicking on the +, and remove", "By checking the box"];
+const todos_work = [];
 
 app.get("/" ,(req, res) => {
     const options = { weekday: "long", day: "numeric", month: "long", year: "numeric"};
     const data = {
         curr_date: new Date().toLocaleDateString("en-GB", options),
-        todos: todos
+        todos: todos_main,
+        came_from: "main"
     };
     res.render("index", data);
 }
@@ -22,10 +23,29 @@ app.get("/" ,(req, res) => {
 
 
 app.post("/" ,(req, res) => {
-    todos.push(req.body.item);
-    res.redirect("/");
+    console.log(req.body.came_from);
+    if (req.body.came_from === "main"){
+        todos_main.push(req.body.item);
+        res.redirect("/");
+    }
+
+    else {
+        todos_work.push(req.body.item);
+        res.redirect("/work");
+    }
+
 });
 
+app.get("/work" ,(req, res) => {
+    const options = { weekday: "long", day: "numeric", month: "long", year: "numeric"};
+    const data = {
+        curr_date: new Date().toLocaleDateString("en-GB", options),
+        todos: todos_work,
+        came_from: "work"
+    };
+    res.render("index", data);
+}
+);
 
 
 app.listen("3000", () => {
